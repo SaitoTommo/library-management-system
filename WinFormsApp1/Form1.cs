6c2ba35cc99618efca9b889e00c0f5b43c69f961
guoto <guoto@SAITOTHINKBOO>
2022-11-19 20:23:04 +0800

    添加项目文件。

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
