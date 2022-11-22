b9a5f4d8ab501334db7fe43457cc7c20da17ea2b
SaitoTommo <guotorre@foxmail.com>
2022-11-22 13:41:28 +0800

    Revert "小修正"
    This reverts commit f77c41351b6b89d078780728624b7bb1399cc81d.

--- WinFormsApp1/dbContext/LibraryDbContext.cs
+++ WinFormsApp1/dbContext/LibraryDbContext.cs
@@ -55,7 +55,6 @@ namespace WinFormsApp1
         public DbSet<Account> Accounts { get; set; }
         public DbSet<BookCategory> Categories { get; set; }
         public DbSet<BorrowLog> BorrowLogs { get; set; }
-        public DbSet<BookWareHouse> BookWareHouses { get; set; }
 
         private readonly string _ConnectionString = string.Empty;
 
